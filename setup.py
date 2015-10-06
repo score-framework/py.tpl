ba580d51608493dbc146c4cda505700c984f6415
Necdet Can Atesman <c.atesman@gmx.net>
2015-10-06 15:07:41 +0200

    Added license and classifiers to setup.py

--- setup.py
+++ setup.py
@@ -39,8 +39,23 @@ setup(
     author='strg.at',
     author_email='score@strg.at',
     url='http://score-framework.org',
-    keywords='score framework web templates jinja pyramid',
+    keywords='score framework web templates jinja jinja2 pyramid',
     packages=['score.tpl', 'score.tpl.jinja2'],
+    license='LGPL',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Framework :: Pyramid',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General '
+            'Public License v3 or later (LGPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+    ],
     install_requires=[
         'score.init >= 0.1'
     ],
