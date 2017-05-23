b2bcb471868b768f628ad147804d59881d852941
Necdet Can Ateşman <can@strg.at>
2017-05-23 14:53:16 +0200

    Release: 0.3.3

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ with open(os.path.join(here, 'README.rst')) as f:
 
 setup(
     name='score.tpl',
-    version='0.3.2',
+    version='0.3.3',
     description='Templating support for The SCORE Framework',
     long_description=README,
     author='strg.at',
