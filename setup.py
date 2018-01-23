980f01eb3f56bf5c70027238da4928a3090473d6
Necdet Can Ateşman <can.atesman@strg.at>
2018-01-23 15:06:08 +0100

    Release: 0.3.7

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Copyright © 2015-2017 STRG.AT GmbH, Vienna, Austria
+# Copyright © 2015-2018 STRG.AT GmbH, Vienna, Austria
 #
 # This file is part of the The SCORE Framework.
 #
@@ -33,7 +33,7 @@ with open(os.path.join(here, 'README.rst')) as f:
 
 setup(
     name='score.tpl',
-    version='0.3.6',
+    version='0.3.7',
     description='Templating support for The SCORE Framework',
     long_description=README,
     author='strg.at',
